180ed9872d6f59fb9483c6cef8b5aa89fc7e4ada
AndresAlbanD <andresalban@unicauca.edu.co>
2024-11-06 22:10:35 -0500

    ContactMeSection update

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,4 @@
-import {Heading, HStack, Image, Text, VStack} from "@chakra-ui/react";
+import {Heading, Image, Text, VStack} from "@chakra-ui/react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faArrowRight} from "@fortawesome/free-solid-svg-icons";
 import React from "react";
@@ -7,18 +7,16 @@ const Card = ({ title, description, imageSrc }) => {
   // Implement the UI for the Card component according to the instructions.
   // You should be able to implement the component with the elements imported above.
   // Feel free to import other UI components from Chakra UI if you wish to.
-  return (
-      <HStack>
-        <VStack borderRadius="xl" boxSize="full" bg="white" padding="4"    >
-          <Image  borderRadius="xl" src={imageSrc}/>
+    return (
+        <VStack borderRadius="xl" boxSize="full" bg="white" padding="4"  cursor='pointer'  >
+            <Image  borderRadius="xl" src={imageSrc}/>
             <VStack color="black" display="flex" alignItems="flex-start">
-            <Heading>{title}</Heading>
-            <Text fontSize="sm" color="gray" noOfLines={2}>{description}</Text>
-            <a>See more <FontAwesomeIcon icon={faArrowRight} size="1x"/></a>
-          </VStack>
+                <Heading>{title}</Heading>
+                <Text fontSize="sm" color="gray" noOfLines={2}>{description}</Text>
+                <a>See more <FontAwesomeIcon icon={faArrowRight} size="1x"/></a>
+            </VStack>
         </VStack>
-      </HStack>
-  );
+    );
 };
 
 export default Card;
--- src/components/ContactMeSection.js
+++ src/components/ContactMeSection.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {useFormik} from "formik";
 import {
   Box,
@@ -12,7 +12,7 @@ import {
   Textarea,
   VStack,
 } from "@chakra-ui/react";
-import * as Yup from 'yup';
+import * as Yup from "yup";
 import FullScreenSection from "./FullScreenSection";
 import useSubmit from "../hooks/useSubmit";
 import {useAlertContext} from "../context/alertContext";
@@ -23,87 +23,84 @@ const LandingSection = () => {
 
   const formik = useFormik({
     initialValues: {
-      firstName: '',
-      email: '',
-      type: '',
-      comment: ''
+      firstName: "",
+      email: "",
+      type: "hireMe",
+      comment: "",
     },
     onSubmit: (values) => {
-      console.log(values)
-      submit("/", values)
-
+      submit('https://john.com/contactme', values)
     },
     validationSchema: Yup.object({
-      firstName: Yup.string().min(1).max(20).required(),
-      email: Yup.string().email().required(),
-      type: Yup.string().oneOf(["hireMe", "openSource", "other"]).required(),
-      comment: Yup.string().min(1).max(300),
+      firstName: Yup.string().required("Required"),
+      email: Yup.string().email("Invalid email address").required("Required"),
+      comment: Yup.string()
+          .min(25, "Must be at least 25 characters")
+          .required("Required"),
     }),
   });
 
+  useEffect(() => {
+    if (response) {
+      onOpen(response.type, response.message);
+      if (response.type === 'success') {
+        formik.resetForm();
+      }
+    }
+  }, [response]);
+
+
+
   return (
-    <FullScreenSection
-      isDarkBackground
-      backgroundColor="#512DA8"
-      py={16}
-      spacing={8}
-    >
-      <VStack w="1024px" p={32} alignItems="flex-start">
-        <Heading as="h1" id="contactme-section">
-          Contact me
-        </Heading>
-        <Box p={6} rounded="md" w="100%">
-          <form onSubmit={formik.handleSubmit}>
-            <VStack spacing={4}>
-              <FormControl isInvalid={false}>
-                <FormLabel htmlFor="firstName">Name</FormLabel>
-                <Input
-                  id="firstName"
-                  name="firstName"
-                  onChange={formik.handleChange}
-                />
-                <FormErrorMessage>{formik.errors.firstName}</FormErrorMessage>
-              </FormControl>
-              <FormControl isInvalid={false}>
-                <FormLabel htmlFor="email">Email Address</FormLabel>
-                <Input
-                  id="email"
-                  name="email"
-                  type="email"
-                  onChange={formik.handleChange}
-                />
-                <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
-              </FormControl>
-              <FormControl>
-                <FormLabel htmlFor="type">Type of enquiry</FormLabel>
-                <Select
-                    id="type"
-                    name="type"
-                    onChange={formik.handleChange}
-                >
-                  <option value="hireMe">Freelance project proposal</option>
-                  <option value="openSource">Open source consultancy session</option>
-                  <option value="other">Other</option>
-                </Select>
-              </FormControl>
-              <FormControl isInvalid={false}>
-                <FormLabel htmlFor="comment">Your message</FormLabel>
-                <Textarea
-                  id="comment"
-                  name="comment"
-                  height={250}
-                  onChange={formik.handleChange}
-                />
-                <FormErrorMessage>{formik.errors.comment}</FormErrorMessage>
-              </FormControl>
-              <Button type="submit" colorScheme="purple" width="full">
-                Submit
-              </Button>
-            </VStack>
-          </form>
-        </Box>
-      </VStack>
-    </FullScreenSection>
+      <FullScreenSection
+          isDarkBackground
+          backgroundColor="#512DA8"
+          py={16}
+          spacing={8}
+      >
+        <VStack w="1024px" p={32} alignItems="flex-start">
+          <Heading as="h1" id="contactme-section">
+            Contact me
+          </Heading>
+          <Box p={6} rounded="md" w="100%">
+            <form onSubmit={formik.handleSubmit}>
+              <VStack spacing={4}>
+                <FormControl isInvalid={!!formik.errors.firstName && formik.touched.firstName}>
+                  <FormLabel htmlFor="firstName">Name</FormLabel>
+                  <Input id="firstName" name="firstName"
+                         {...formik.getFieldProps('firstName')}/>
+                  <FormErrorMessage>{formik.errors.firstName}</FormErrorMessage>
+                </FormControl>
+                <FormControl isInvalid={!!formik.errors.email && formik.touched.email}>
+                  <FormLabel htmlFor="email">Email Address</FormLabel>
+                  <Input id="email" name="email" type="email" {...formik.getFieldProps('email')}/>
+                  <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
+                </FormControl>
+                <FormControl>
+                  <FormLabel htmlFor="type">Type of enquiry</FormLabel>
+                  <Select id="type" name="type"
+                          {...formik.getFieldProps("type")}>
+                    <option value="hireMe">Freelance project proposal</option>
+                    <option value="openSource">
+                      Open source consultancy session
+                    </option>
+                    <option value="other">Other</option>
+                  </Select>
+                </FormControl>
+                <FormControl isInvalid={!!formik.errors.comment && formik.touched.comment}>
+                  <FormLabel htmlFor="comment">Your message</FormLabel>
+                  <Textarea id="comment" name="comment" height={250}
+                            {...formik.getFieldProps("comment")}/>
+                  <FormErrorMessage>{formik.errors.comment}</FormErrorMessage>
+                </FormControl>
+                <Button type="submit" colorScheme="purple" width="full" isLoading={isLoading}>
+                  Submit
+                </Button>
+              </VStack>
+            </form>
+          </Box>
+        </VStack>
+      </FullScreenSection>
   );
 };
 
